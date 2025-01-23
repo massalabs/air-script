294479ec1582daedd6c028d0c87c16f9468a98c7
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2025-01-23 17:34:59 +0100

    fix Parameter PartialEq and Hash to work on disconnected identical graphs

--- ir/src/ir/nodes/roots/mod.rs
+++ ir/src/ir/nodes/roots/mod.rs
@@ -1,12 +1,14 @@
 mod evaluator;
 mod function;
+use std::hash::{Hash, Hasher};
+
 pub use evaluator::Evaluator;
 pub use function::Function;
 
 use super::MirType;
 use crate::ir::{BackLink, Builder, Child, Link, Node, Op, Owner};
 
-#[derive(Builder, Default, Clone, PartialEq, Eq, Debug, Hash)]
+#[derive(Builder, Default, Clone, Eq, Debug)]
 #[enum_wrapper(Op)]
 pub struct Parameter {
     parents: Vec<BackLink<Owner>>,
@@ -30,6 +32,28 @@ impl Parameter {
 
     pub fn set_ref_node(&mut self, ref_node: Link<Owner>) {
         self.ref_node = ref_node.into();
+    }
+}
+
+fn get_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = std::hash::DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
+impl PartialEq for Parameter {
+    fn eq(&self, other: &Self) -> bool {
+        self.position == other.position
+            && self.ty == other.ty
+            && get_hash(&self.ref_node) == get_hash(&other.ref_node)
+    }
+}
+
+impl Hash for Parameter {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.position.hash(state);
+        self.ty.hash(state);
+        self.ref_node.hash(state);
     }
 }
 
