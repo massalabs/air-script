adf6056436d26478df81bb9980e84b33d8e1b0c4
Leo-Besancon <lb@massa.net>
2025-01-24 16:16:45 +0100

    Fix let translation

--- ir/src/passes/translate.rs
+++ ir/src/passes/translate.rs
@@ -369,13 +369,14 @@ impl<'a> MirBuilder<'a> {
     fn translate_let(&mut self, let_stmt: &'a ast::Let) -> Result<Link<Op>, CompileError> {
         let name = &let_stmt.name;
         let value: Link<Op> = self.translate_expr(&let_stmt.value)?;
+        let mut ret_value = value.clone();
         self.bindings.enter();
         self.bindings.insert(name, value.clone());
         for stmt in let_stmt.body.iter() {
-            self.translate_statement(stmt)?;
+            ret_value = self.translate_statement(stmt)?;
         }
         self.bindings.exit();
-        Ok(value)
+        Ok(ret_value)
     }
     fn translate_expr(&mut self, expr: &'a ast::Expr) -> Result<Link<Op>, CompileError> {
         match expr {
