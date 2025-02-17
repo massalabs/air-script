b7f932f3f7fc9c608c1ee7d4227d60e7aabc9c65
Leo-Besancon <lb@massa.net>
2025-02-17 23:43:45 +0100

    feat: add bus operation expand in MIR

--- mir/src/passes/bus_op_expand.rs
+++ mir/src/passes/bus_op_expand.rs
@@ -1,12 +1,12 @@
-use std::ops::Deref;
-
 use air_parser::ast::{AccessType, BusType};
 use air_pass::Pass;
 use miden_diagnostics::{DiagnosticsHandler, SourceSpan, Spanned};
 
-use super::{duplicate_node, visitor::Visitor};
+use super::duplicate_node;
 use crate::{
-    ir::{Accessor, Graph, Link, Mir, MirValue, Mul, Node, Op, SpannedMirValue, Sub, Value},
+    ir::{
+        Accessor, Add, BusOpKind, Enf, Link, Mir, MirValue, Mul, Node, SpannedMirValue, Sub, Value,
+    },
     CompileError,
 };
 
@@ -17,7 +17,7 @@ use crate::{
 pub struct BusOpExpand<'a> {
     #[allow(unused)]
     diagnostics: &'a DiagnosticsHandler,
-    work_stack: Vec<Link<Node>>,
+    _work_stack: Vec<Link<Node>>,
 }
 
 impl Pass for BusOpExpand<'_> {
@@ -30,20 +30,20 @@ impl Pass for BusOpExpand<'_> {
 
         let buses = graph.buses.clone();
 
-        for (ident, bus) in buses {
+        for (_ident, bus) in buses {
             let bus_type = bus.borrow().bus_type.clone();
             let columns = bus.borrow().columns.clone(); // columns are the bus_operations (add or remove of a Vec of arguments)
             let latches = bus.borrow().latches.clone(); // latches are the selectors
 
             let bus_access = Value::create(SpannedMirValue {
-                span: bus.borrow().span().clone(),
+                span: bus.borrow().span(),
                 value: MirValue::BusAccess(bus.clone()),
             });
             let bus_access_with_offset = Accessor::create(
                 duplicate_node(bus_access.clone(), &mut Default::default()),
                 AccessType::Default,
                 1,
-                bus.borrow().span().clone(),
+                bus.borrow().span(),
             );
 
             match bus_type {
@@ -65,28 +65,212 @@ impl Pass for BusOpExpand<'_> {
                     });
 
                     for (column, latch) in columns.iter().zip(latches.iter()) {
-
                         let bus_op = column.as_bus_op().unwrap();
                         let bus_op_kind = bus_op.kind.clone();
                         let bus_op_args = bus_op.args.clone();
 
                         // 1. Combine args with alphas
-                        // 2. multiply with latch
-                        // 3. add inverse of latch
-                        // 4. add to p_factor or p_prime_factor (depending on bus_op_kind: add: p, rem: p_prime)
+                        // 1.1 Start with the first alpha
+                        let mut args_combined = Value::create(SpannedMirValue {
+                            span: SourceSpan::default(),
+                            value: MirValue::RandomValue(0),
+                        });
+                        for (index, arg) in bus_op_args.iter().enumerate() {
+                            // 1.2 Create corresponding alpha
+                            let alpha = Value::create(SpannedMirValue {
+                                span: SourceSpan::default(),
+                                value: MirValue::RandomValue(index + 1),
+                            });
 
+                            // 1.3 Multiply arg with alpha
+                            let arg_times_alpha =
+                                Mul::create(arg.clone(), alpha, SourceSpan::default());
+
+                            // 1.4 Combine with other args
+                            args_combined =
+                                Add::create(args_combined, arg_times_alpha, SourceSpan::default());
+                        }
+
+                        // 2. Multiply by latch
+                        let args_combined_with_latch =
+                            Mul::create(args_combined, latch.clone(), SourceSpan::default());
+
+                        // 3. add inverse of latch
+                        let args_combined_with_latch_and_latch_inverse = Add::create(
+                            args_combined_with_latch,
+                            Sub::create(
+                                Value::create(SpannedMirValue {
+                                    span: SourceSpan::default(),
+                                    value: MirValue::Constant(crate::ir::ConstantValue::Felt(1)),
+                                }),
+                                duplicate_node(latch.clone(), &mut Default::default()),
+                                SourceSpan::default(),
+                            ),
+                            SourceSpan::default(),
+                        );
+
+                        // 4. add to p_factor or p_prime_factor (depending on bus_op_kind: add: p, rem: p_prime)
+                        match bus_op_kind {
+                            BusOpKind::Add => {
+                                p_factor = Add::create(
+                                    p_factor,
+                                    args_combined_with_latch_and_latch_inverse,
+                                    SourceSpan::default(),
+                                );
+                            }
+                            BusOpKind::Rem => {
+                                p_prime_factor = Add::create(
+                                    p_prime_factor,
+                                    args_combined_with_latch_and_latch_inverse,
+                                    SourceSpan::default(),
+                                );
+                            }
+                        }
                     }
 
+                    // 5. Multiply the factors with the bus column (with and without offset for p' and p respectively)
                     let p_prod = Mul::create(p_factor, bus_access, SourceSpan::default());
                     let p_prime_prod = Mul::create(
                         p_prime_factor,
                         bus_access_with_offset,
                         SourceSpan::default(),
                     );
+
+                    // 6. Create the resulting constraint and insert it into the graph
+                    let resulting_constraint = Enf::create(
+                        Sub::create(p_prod, p_prime_prod, SourceSpan::default()),
+                        SourceSpan::default(),
+                    );
+
+                    graph.insert_integrity_constraints_root(resulting_constraint);
                 }
                 BusType::Mult => {
-                    //
-                    for (columns, latches) in columns.iter().zip(latches.iter()) {}
+                    // Example:
+                    // q.add(a, b, c) for d
+                    // q.rem(e, f, g) when s
+                    // => q' + s / ( A0 + A1 e + A2 f + A3 g ) = q + d / ( A0 + A1 a + A2 b + A3 c )
+
+                    //  q' + s / ( columns removed combined with alphas ) = q + d / ( columns added combined with alphas )
+                    // PROD * q' + s * ( columns added combined with alphas ) = PROD * q + d * ( columns removed combined with alphas )
+
+                    // 1. Compute all the factors
+                    let mut factors = vec![];
+                    for column in columns.iter() {
+                        let bus_op = column.as_bus_op().unwrap();
+                        let bus_op_args = bus_op.args.clone();
+
+                        // 1. Combine args with alphas
+                        // 1.1 Start with the first alpha
+                        let mut args_combined = Value::create(SpannedMirValue {
+                            span: SourceSpan::default(),
+                            value: MirValue::RandomValue(0),
+                        });
+                        for (index, arg) in bus_op_args.iter().enumerate() {
+                            // 1.2 Create corresponding alpha
+                            let alpha = Value::create(SpannedMirValue {
+                                span: SourceSpan::default(),
+                                value: MirValue::RandomValue(index + 1),
+                            });
+
+                            // 1.3 Multiply arg with alpha
+                            let arg_times_alpha =
+                                Mul::create(arg.clone(), alpha, SourceSpan::default());
+
+                            // 1.4 Combine with other args
+                            args_combined =
+                                Add::create(args_combined, arg_times_alpha, SourceSpan::default());
+                        }
+
+                        factors.push(args_combined);
+                    }
+
+                    // 2. Compute the product of all factors (will be used to mult q and q')
+                    let mut total_factors = Value::create(SpannedMirValue {
+                        span: SourceSpan::default(),
+                        value: MirValue::Constant(crate::ir::ConstantValue::Felt(1)),
+                    });
+                    for factor in factors.iter() {
+                        total_factors =
+                            Mul::create(total_factors, factor.clone(), SourceSpan::default());
+                    }
+
+                    // 3. For each column, compute the product of all factors except the one of the current column, and multiply it with the latch
+                    let mut terms_added_to_bus = Value::create(SpannedMirValue {
+                        span: SourceSpan::default(),
+                        value: MirValue::Constant(crate::ir::ConstantValue::Felt(0)),
+                    });
+                    let mut terms_removed_from_bus = Value::create(SpannedMirValue {
+                        span: SourceSpan::default(),
+                        value: MirValue::Constant(crate::ir::ConstantValue::Felt(0)),
+                    });
+                    for (index, (column, latch)) in columns.iter().zip(latches.iter()).enumerate() {
+                        let bus_op = column.as_bus_op().unwrap();
+                        let bus_op_kind = bus_op.kind.clone();
+
+                        // 3.1 Compute the product of all factors except the one of the current column
+                        let mut factors_without_current = Value::create(SpannedMirValue {
+                            span: SourceSpan::default(),
+                            value: MirValue::Constant(crate::ir::ConstantValue::Felt(1)),
+                        });
+                        for (i, factor) in factors.iter().enumerate() {
+                            if i != index {
+                                factors_without_current = Mul::create(
+                                    factors_without_current,
+                                    factor.clone(),
+                                    SourceSpan::default(),
+                                );
+                            }
+                        }
+
+                        // 3.2 Multiply by latch
+                        let factors_without_current_with_latch = Mul::create(
+                            factors_without_current,
+                            latch.clone(),
+                            SourceSpan::default(),
+                        );
+
+                        // 3.3 Depending on the bus_op_kind, add to q_factor or q_prime_factor
+                        match bus_op_kind {
+                            BusOpKind::Add => {
+                                terms_added_to_bus = Add::create(
+                                    terms_added_to_bus,
+                                    factors_without_current_with_latch,
+                                    SourceSpan::default(),
+                                );
+                            }
+                            BusOpKind::Rem => {
+                                terms_removed_from_bus = Add::create(
+                                    terms_removed_from_bus,
+                                    factors_without_current_with_latch,
+                                    SourceSpan::default(),
+                                );
+                            }
+                        }
+                    }
+
+                    // 4. Add all the terms together
+                    let q_prod =
+                        Mul::create(total_factors.clone(), bus_access, SourceSpan::default());
+                    let q_prime_prod = Mul::create(
+                        total_factors.clone(),
+                        bus_access_with_offset,
+                        SourceSpan::default(),
+                    );
+                    let q_term =
+                        Add::create(q_prod, terms_added_to_bus.clone(), SourceSpan::default());
+                    let q_prime_term = Add::create(
+                        q_prime_prod,
+                        terms_removed_from_bus.clone(),
+                        SourceSpan::default(),
+                    );
+
+                    // 5. Create the resulting constraint and insert it into the graph
+                    let resulting_constraint = Enf::create(
+                        Sub::create(q_term, q_prime_term, SourceSpan::default()),
+                        SourceSpan::default(),
+                    );
+
+                    graph.insert_integrity_constraints_root(resulting_constraint);
                 }
             }
         }
@@ -95,7 +279,7 @@ impl Pass for BusOpExpand<'_> {
     }
 }
 
-impl<'a> BusOpExpand<'a> {
+/*impl<'a> BusOpExpand<'a> {
     #[allow(unused)]
     pub fn new(diagnostics: &'a DiagnosticsHandler) -> Self {
         Self {
@@ -155,4 +339,4 @@ impl Visitor for BusOpExpand<'_> {
 
         Ok(())
     }
-}
+}*/
