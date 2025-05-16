a187d99cfa72a8a95ff2bb0a0e58b493ada20a86
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2025-05-16 16:15:09 +0200

    implement constant propagation

--- mir/src/passes/constant_propagation.rs
+++ mir/src/passes/constant_propagation.rs
@@ -1,12 +1,15 @@
-use air_parser::{ast::Identifier, LexicalScope};
+use air_parser::{
+    ast::{AccessType, Identifier},
+    LexicalScope,
+};
 use air_pass::Pass;
 use miden_diagnostics::DiagnosticsHandler;
 
-use super::visitor::Visitor;
+use super::{duplicate_node, visitor::Visitor};
 use crate::{
     ir::{
-        extract_all_roots, Graph, Link, Mir, MirValue, Node, Op, Parent, Root, SpannedMirValue,
-        Value,
+        extract_all_roots, Add, Call, ConstantValue, Exp, Fold, For, Graph, If, Link, Matrix, Mir,
+        MirValue, Mul, Node, Op, Owner, Parent, Root, SpannedMirValue, Sub, Value, Vector,
     },
     CompileError,
 };
@@ -23,7 +26,6 @@ pub struct ConstantPropagation<'a> {
     #[allow(unused)]
     diagnostics: &'a DiagnosticsHandler,
     work_stack: Vec<Link<Node>>,
-    bindings: LexicalScope<Identifier, Link<Op>>,
 }
 
 impl Pass for ConstantPropagation<'_> {
@@ -33,6 +35,8 @@ impl Pass for ConstantPropagation<'_> {
 
     fn run<'a>(&mut self, mut ir: Self::Input<'a>) -> Result<Self::Output<'a>, Self::Error> {
         Visitor::run(self, ir.constraint_graph_mut())?;
+        ir.constraint_graph_mut().constants.clear();
+        dbg!(&ir);
         Ok(ir)
     }
 }
@@ -43,8 +47,21 @@ impl<'a> ConstantPropagation<'a> {
         Self {
             diagnostics,
             work_stack: Default::default(),
-            bindings: Default::default(),
         }
+    }
+    fn replace_constant_children(
+        &mut self,
+        graph: &mut Graph,
+        node: Link<Node>,
+    ) -> Result<(), CompileError> {
+        debug(&node);
+        let children = node.children();
+        for child in children.borrow().iter() {
+            if has_constant(&child.as_node()) {
+                self.visit_node(graph, child.as_node())?;
+            }
+        }
+        Ok(())
     }
 }
 
@@ -64,94 +81,184 @@ impl Visitor for ConstantPropagation<'_> {
     }
     fn visit_function(
         &mut self,
-        _graph: &mut Graph,
+        graph: &mut Graph,
         function: Link<Root>,
     ) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, function.as_node())?;
         debug(&function.as_node());
         Ok(())
     }
     fn visit_evaluator(
         &mut self,
-        _graph: &mut Graph,
+        graph: &mut Graph,
         evaluator: Link<Root>,
     ) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, evaluator.as_node())?;
         debug(&evaluator.as_node());
         Ok(())
     }
-    fn visit_enf(&mut self, _graph: &mut Graph, enf: Link<Op>) -> Result<(), CompileError> {
-        debug(&enf.as_node());
+    fn visit_enf(&mut self, graph: &mut Graph, enf: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, enf.as_node())?;
         Ok(())
     }
     fn visit_boundary(
         &mut self,
-        _graph: &mut Graph,
+        graph: &mut Graph,
         boundary: Link<Op>,
     ) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, boundary.as_node())?;
         debug(&boundary.as_node());
         Ok(())
     }
-    fn visit_add(&mut self, _graph: &mut Graph, add: Link<Op>) -> Result<(), CompileError> {
-        debug(&add.as_node());
+    fn visit_add(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, op.as_node())?;
+        if !is_constant(&op.as_node()) {
+            // This is not a constant, so we don't need to replace it
+            return Ok(());
+        }
+        let orig = duplicate_node(op.clone(), &mut Default::default());
+        if let Some(add) = orig.as_add() {
+            if let (Some(lhs), Some(rhs)) = (as_constant(&add.lhs), as_constant(&add.rhs)) {
+                op.update(&(lhs + rhs).into());
+            };
+        }
+        debug(&op.as_node());
         Ok(())
     }
-    fn visit_sub(&mut self, _graph: &mut Graph, sub: Link<Op>) -> Result<(), CompileError> {
-        debug(&sub.as_node());
+    fn visit_sub(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, op.as_node())?;
+        if !is_constant(&op.as_node()) {
+            // This is not a constant, so we don't need to replace it
+            return Ok(());
+        }
+        let orig = duplicate_node(op.clone(), &mut Default::default());
+        if let Some(sub) = orig.as_sub() {
+            if let (Some(lhs), Some(rhs)) = (as_constant(&sub.lhs), as_constant(&sub.rhs)) {
+                op.update(&(lhs - rhs).into());
+            };
+        }
+        debug(&op.as_node());
         Ok(())
     }
-    fn visit_mul(&mut self, _graph: &mut Graph, mul: Link<Op>) -> Result<(), CompileError> {
-        debug(&mul.as_node());
+    fn visit_mul(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, op.as_node())?;
+        if !is_constant(&op.as_node()) {
+            // This is not a constant, so we don't need to replace it
+            return Ok(());
+        }
+        let orig = duplicate_node(op.clone(), &mut Default::default());
+        if let Some(mul) = orig.as_mul() {
+            if let (Some(lhs), Some(rhs)) = (as_constant(&mul.lhs), as_constant(&mul.rhs)) {
+                op.update(&(lhs * rhs).into());
+            };
+        }
+        debug(&op.as_node());
         Ok(())
     }
-    fn visit_exp(&mut self, _graph: &mut Graph, exp: Link<Op>) -> Result<(), CompileError> {
-        debug(&exp.as_node());
+    fn visit_exp(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, op.as_node())?;
+        if !is_constant(&op.as_node()) {
+            // This is not a constant, so we don't need to replace it
+            return Ok(());
+        }
+        let orig = duplicate_node(op.clone(), &mut Default::default());
+        if let Some(mul) = orig.as_exp() {
+            if let (Some(lhs), Some(rhs)) = (as_constant(&mul.lhs), as_constant(&mul.rhs)) {
+                assert!(rhs < 2_u64.pow(32));
+                op.update(&(lhs.pow(rhs as u32)).into());
+            };
+        }
+        debug(&op.as_node());
         Ok(())
     }
-    fn visit_if(&mut self, _graph: &mut Graph, if_node: Link<Op>) -> Result<(), CompileError> {
-        debug(&if_node.as_node());
+    fn visit_if(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, op.as_node())?;
         Ok(())
     }
-    fn visit_for(&mut self, _graph: &mut Graph, for_node: Link<Op>) -> Result<(), CompileError> {
-        debug(&for_node.as_node());
+    fn visit_for(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, op.as_node())?;
         Ok(())
     }
-    fn visit_call(&mut self, _graph: &mut Graph, call: Link<Op>) -> Result<(), CompileError> {
-        debug(&call.as_node());
+    fn visit_call(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, op.as_node())?;
         Ok(())
     }
-    fn visit_fold(&mut self, _graph: &mut Graph, fold: Link<Op>) -> Result<(), CompileError> {
-        debug(&fold.as_node());
+    fn visit_fold(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, op.as_node())?;
         Ok(())
     }
-    fn visit_vector(&mut self, _graph: &mut Graph, vector: Link<Op>) -> Result<(), CompileError> {
-        debug(&vector.as_node());
+    fn visit_vector(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, op.as_node())?;
         Ok(())
     }
-    fn visit_matrix(&mut self, _graph: &mut Graph, matrix: Link<Op>) -> Result<(), CompileError> {
-        debug(&matrix.as_node());
+    fn visit_matrix(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, op.as_node())?;
         Ok(())
     }
-    fn visit_accessor(
-        &mut self,
-        _graph: &mut Graph,
-        accessor: Link<Op>,
-    ) -> Result<(), CompileError> {
-        debug(&accessor.as_node());
+    fn visit_accessor(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, op.as_node())?;
+        if !is_constant(&op.as_node()) {
+            // This is not a constant, so we don't need to replace it
+            return Ok(());
+        }
+        let orig = duplicate_node(op.clone(), &mut Default::default());
+        if let Some(accessor) = orig.as_accessor() {
+            let indexable = accessor.indexable.clone();
+            match (
+                accessor.access_type.clone(),
+                accessor.indexable.borrow().deref(),
+            ) {
+                (AccessType::Default, _) => {
+                    if let Some(value) = as_constant(&indexable) {
+                        op.update(&value.into());
+                    }
+                }
+                (AccessType::Slice(_), _) => {
+                    unimplemented!("Slice accessors are not supported yet");
+                }
+                (AccessType::Index(index), Op::Vector(v)) => {
+                    let value = v
+                        .get_element(index)
+                        .unwrap_or_else(|| panic!("Index out of bounds: {} >= {}", index, v.size));
+                    if let Some(value) = as_constant(&value) {
+                        op.update(&value.into());
+                    }
+                }
+                (AccessType::Index(index), Op::Matrix(m)) => {
+                    let value = m
+                        .get_element(index)
+                        .unwrap_or_else(|| panic!("Index out of bounds: {} >= {}", index, m.size));
+                    if let Some(value) = as_constant(&value) {
+                        op.update(&value.into());
+                    }
+                }
+                (AccessType::Matrix(row_idx, col_idx), Op::Matrix(m)) => {
+                    let row = m.get_element(row_idx).unwrap_or_else(|| {
+                        panic!("Index out of bounds: {} >= {}", row_idx, m.size)
+                    });
+                    let vector = row
+                        .as_vector()
+                        .unwrap_or_else(|| panic!("Expected a vector, got: {:?}", m));
+                    let value = vector.get_element(col_idx).unwrap_or_else(|| {
+                        panic!("Index out of bounds: {} >= {}", col_idx, m.size)
+                    });
+                    if let Some(value) = as_constant(&value) {
+                        op.update(&value.into());
+                    }
+                }
+                _ => {
+                    unimplemented!(
+                        "Unsupported access type: {:?} for {:?}",
+                        accessor.access_type,
+                        indexable
+                    );
+                }
+            };
+        }
         Ok(())
     }
-    fn visit_bus_op(&mut self, _graph: &mut Graph, bus_op: Link<Op>) -> Result<(), CompileError> {
-        debug(&bus_op.as_node());
-        Ok(())
-    }
-    fn visit_parameter(
-        &mut self,
-        _graph: &mut Graph,
-        parameter: Link<Op>,
-    ) -> Result<(), CompileError> {
-        debug(&parameter.as_node());
-        Ok(())
-    }
-    fn visit_value(&mut self, _graph: &mut Graph, value: Link<Op>) -> Result<(), CompileError> {
-        debug(&value.as_node());
+    fn visit_bus_op(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, op.as_node())?;
         Ok(())
     }
 }
@@ -225,4 +332,22 @@ fn debug(node: &Link<Node>) {
         has_constant(node),
         node.debug(),
     );
+}
+
+fn as_constant(op: &Link<Op>) -> Option<u64> {
+    op.as_value().map(|v| {
+        if let Value {
+            value:
+                SpannedMirValue {
+                    value: MirValue::Constant(ConstantValue::Felt(value)),
+                    ..
+                },
+            ..
+        } = v.deref()
+        {
+            Some(*value)
+        } else {
+            None
+        }
+    })?
 }
