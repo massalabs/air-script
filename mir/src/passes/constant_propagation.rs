8e4e25d8f8d0785a799223279d13758e696125b8
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2025-05-22 17:41:46 +0200

    better debugging, simplify logic, fix index accessor on matrix

--- mir/src/passes/constant_propagation.rs
+++ mir/src/passes/constant_propagation.rs
@@ -5,8 +5,8 @@ use miden_diagnostics::DiagnosticsHandler;
 use super::{duplicate_node, visitor::Visitor};
 use crate::{
     ir::{
-        extract_all_roots, ConstantValue, Graph, Link, Mir, MirValue, Node, Op, Parent, Root,
-        SpannedMirValue, Value,
+        extract_all_roots, Accessor, ConstantValue, Graph, Link, Mir, MirValue, Node, Op,
+        Parameter, Parent, SpannedMirValue, Value,
     },
     CompileError,
 };
@@ -23,6 +23,7 @@ pub struct ConstantPropagation<'a> {
     #[allow(unused)]
     diagnostics: &'a DiagnosticsHandler,
     work_stack: Vec<Link<Node>>,
+    indent: isize,
 }
 
 impl Pass for ConstantPropagation<'_> {
@@ -33,7 +34,6 @@ impl Pass for ConstantPropagation<'_> {
     fn run<'a>(&mut self, mut ir: Self::Input<'a>) -> Result<Self::Output<'a>, Self::Error> {
         Visitor::run(self, ir.constraint_graph_mut())?;
         ir.constraint_graph_mut().constants.clear();
-        dbg!(&ir);
         Ok(ir)
     }
 }
@@ -44,6 +44,7 @@ impl<'a> ConstantPropagation<'a> {
         Self {
             diagnostics,
             work_stack: Default::default(),
+            indent: 0,
         }
     }
     fn replace_constant_children(
@@ -51,7 +52,7 @@ impl<'a> ConstantPropagation<'a> {
         graph: &mut Graph,
         node: Link<Node>,
     ) -> Result<(), CompileError> {
-        debug(&node);
+        self.debug(1, format!("{:?} ", node).as_str(), &node);
         let children = node.children();
         for child in children.borrow().iter() {
             if has_constant(&child.as_node()) {
@@ -60,11 +61,38 @@ impl<'a> ConstantPropagation<'a> {
         }
         Ok(())
     }
+    fn debug(&mut self, indent: isize, prefix: &str, node: &Link<Node>) {
+        if indent < 0 {
+            if self.indent < 0 {
+                self.indent = 0;
+            } else {
+                self.indent += indent;
+            }
+        }
+        let prefix = " ".repeat((self.indent * 4) as usize) + prefix;
+        eprintln!(
+            "{}is_constant: {}, has_constant: {}, node: {}",
+            prefix,
+            is_constant(node),
+            has_constant(node),
+            node.debug(),
+        );
+        if indent > 0 {
+            self.indent += indent;
+        }
+    }
 }
 
 impl Visitor for ConstantPropagation<'_> {
     fn work_stack(&mut self) -> &mut Vec<Link<Node>> {
         &mut self.work_stack
+    }
+    fn pre_visit(&mut self, graph: &mut Graph, node: Link<Node>) -> Result<(), CompileError> {
+        self.replace_constant_children(graph, node.clone())
+    }
+    fn post_visit(&mut self, _graph: &mut Graph, node: Link<Node>) -> Result<(), CompileError> {
+        self.debug(-1, "  -> ", &node);
+        Ok(())
     }
     fn root_nodes_to_visit(
         &self,
@@ -76,39 +104,7 @@ impl Visitor for ConstantPropagation<'_> {
             .cloned()
             .collect()
     }
-    fn visit_function(
-        &mut self,
-        graph: &mut Graph,
-        function: Link<Root>,
-    ) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, function.as_node())?;
-        debug(&function.as_node());
-        Ok(())
-    }
-    fn visit_evaluator(
-        &mut self,
-        graph: &mut Graph,
-        evaluator: Link<Root>,
-    ) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, evaluator.as_node())?;
-        debug(&evaluator.as_node());
-        Ok(())
-    }
-    fn visit_enf(&mut self, graph: &mut Graph, enf: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, enf.as_node())?;
-        Ok(())
-    }
-    fn visit_boundary(
-        &mut self,
-        graph: &mut Graph,
-        boundary: Link<Op>,
-    ) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, boundary.as_node())?;
-        debug(&boundary.as_node());
-        Ok(())
-    }
-    fn visit_add(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, op.as_node())?;
+    fn visit_add(&mut self, _graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
         if !is_constant(&op.as_node()) {
             // This is not a constant, so we don't need to replace it
             return Ok(());
@@ -121,11 +117,9 @@ impl Visitor for ConstantPropagation<'_> {
                 op.update(&new);
             };
         }
-        debug(&op.as_node());
         Ok(())
     }
-    fn visit_sub(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, op.as_node())?;
+    fn visit_sub(&mut self, _graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
         if !is_constant(&op.as_node()) {
             // This is not a constant, so we don't need to replace it
             return Ok(());
@@ -138,11 +132,9 @@ impl Visitor for ConstantPropagation<'_> {
                 op.update(&new);
             };
         }
-        debug(&op.as_node());
         Ok(())
     }
-    fn visit_mul(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, op.as_node())?;
+    fn visit_mul(&mut self, _graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
         if !is_constant(&op.as_node()) {
             // This is not a constant, so we don't need to replace it
             return Ok(());
@@ -155,11 +147,9 @@ impl Visitor for ConstantPropagation<'_> {
                 op.update(&new);
             };
         }
-        debug(&op.as_node());
         Ok(())
     }
-    fn visit_exp(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, op.as_node())?;
+    fn visit_exp(&mut self, _graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
         if !is_constant(&op.as_node()) {
             // This is not a constant, so we don't need to replace it
             return Ok(());
@@ -173,35 +163,9 @@ impl Visitor for ConstantPropagation<'_> {
                 op.update(&new);
             };
         }
-        debug(&op.as_node());
-        Ok(())
-    }
-    fn visit_if(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, op.as_node())?;
-        Ok(())
-    }
-    fn visit_for(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, op.as_node())?;
-        Ok(())
-    }
-    fn visit_call(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, op.as_node())?;
-        Ok(())
-    }
-    fn visit_fold(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, op.as_node())?;
-        Ok(())
-    }
-    fn visit_vector(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, op.as_node())?;
-        Ok(())
-    }
-    fn visit_matrix(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, op.as_node())?;
         Ok(())
     }
     fn visit_accessor(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, op.as_node())?;
         if !is_constant(&op.as_node()) {
             // This is not a constant, so we don't need to replace it
             return Ok(());
@@ -233,22 +197,6 @@ impl Visitor for ConstantPropagation<'_> {
                         op.update(&new);
                     }
                 }
-                (AccessType::Index(index), Op::Matrix(m)) => {
-                    let value = m
-                        .get_element(index)
-                        .unwrap_or_else(|| panic!("Index out of bounds: {} >= {}", index, m.size));
-                    if !is_constant(&value.as_node()) {
-                        // This is not a constant, so we don't need to replace it
-                        return Ok(());
-                    }
-                    // TODO: Fix this, currently doesnt work it extracts a constant
-                    // instead of a constant vector
-                    if let Some(value) = as_constant(&value) {
-                        let new: Link<Op> = value.into();
-                        new.as_value_mut().unwrap().value.span = accessor.span;
-                        op.update(&new);
-                    }
-                }
                 (AccessType::Matrix(row_idx, col_idx), Op::Matrix(m)) => {
                     let row = m.get_element(row_idx).unwrap_or_else(|| {
                         panic!("Index out of bounds: {} >= {}", row_idx, m.size)
@@ -265,6 +213,19 @@ impl Visitor for ConstantPropagation<'_> {
                         op.update(&new);
                     }
                 }
+                (AccessType::Index(index), Op::Matrix(m)) => {
+                    let value = m
+                        .get_element(index)
+                        .unwrap_or_else(|| panic!("Index out of bounds: {} >= {}", index, m.size));
+                    if !is_constant(&value.as_node()) {
+                        // This is not a constant, so we don't need to replace it
+                        return Ok(());
+                    }
+                    let new_accessor =
+                        Accessor::create(value.clone(), AccessType::Default, 0, accessor.span);
+                    op.update(&new_accessor);
+                    self.visit_node(graph, new_accessor.as_node())?;
+                }
                 _ => {
                     unimplemented!(
                         "Unsupported access type: {:?} for {:?}",
@@ -276,81 +237,98 @@ impl Visitor for ConstantPropagation<'_> {
         }
         Ok(())
     }
-    fn visit_bus_op(&mut self, graph: &mut Graph, op: Link<Op>) -> Result<(), CompileError> {
-        self.replace_constant_children(graph, op.as_node())?;
-        Ok(())
+}
+
+const DO_ALL: bool = true;
+const DO_ANY: bool = false;
+fn is_or_has_constant<F>(f: F, all: bool, node: &Link<Node>) -> bool
+where
+    F: Fn(&Link<Node>) -> bool,
+{
+    if node.is_parent() {
+        if all {
+            return node
+                .children()
+                .borrow()
+                .iter()
+                .all(|child| f(&child.as_node()));
+        } else {
+            return node
+                .children()
+                .borrow()
+                .iter()
+                .any(|child| f(&child.as_node()));
+        }
+    } else if let Some(op) = node.as_op() {
+        match op.borrow().deref() {
+            Op::Value(Value {
+                value:
+                    SpannedMirValue {
+                        value: MirValue::Constant(_),
+                        ..
+                    },
+                ..
+            }) => {
+                // This is a constant
+                return true;
+            }
+            Op::Parameter(Parameter { ref_node, .. }) => {
+                let Some(ref_node_link) = ref_node.to_link() else {
+                    // This is a stale node
+                    return false;
+                };
+                let Some(ref_op) = ref_node_link.as_op() else {
+                    return false;
+                };
+                match ref_op.borrow().deref() {
+                    Op::For(for_ref) => {
+                        if all {
+                            return for_ref
+                                .iterators
+                                .borrow()
+                                .iter()
+                                .all(|iterator| f(&iterator.as_node()));
+                        } else {
+                            return for_ref
+                                .iterators
+                                .borrow()
+                                .iter()
+                                .any(|iterator| f(&iterator.as_node()));
+                        }
+                    }
+                    Op::Call(call_ref) => {
+                        if all {
+                            return call_ref
+                                .arguments
+                                .borrow()
+                                .iter()
+                                .all(|arg| f(&arg.as_node()));
+                        } else {
+                            return call_ref
+                                .arguments
+                                .borrow()
+                                .iter()
+                                .any(|arg| f(&arg.as_node()));
+                        }
+                    }
+                    _ => {
+                        // This has no reference to a potential constant
+                        return false;
+                    }
+                };
+            }
+            _ => return false,
+        }
     }
+    // This is a stale node
+    false
 }
 
 fn is_constant(node: &Link<Node>) -> bool {
-    if node.is_parent() {
-        return node
-            .children()
-            .borrow()
-            .iter()
-            .all(|child| is_constant(&child.as_node()));
-    } else if let Some(op) = node.as_op() {
-        if let Some(v) = op.as_value() {
-            if let Value {
-                value:
-                    SpannedMirValue {
-                        value: MirValue::Constant(_),
-                        ..
-                    },
-                ..
-            } = v.deref()
-            {
-                // This is a constant
-                return true;
-            }
-            // This is a value that is not a constant
-            return false;
-        }
-        // This is an operation that is not a value, nor a parent
-        return false;
-    }
-    // This is a stale node
-    false
+    is_or_has_constant(is_constant, DO_ALL, node)
 }
-
 fn has_constant(node: &Link<Node>) -> bool {
-    if node.is_parent() {
-        return node
-            .children()
-            .borrow()
-            .iter()
-            .any(|child| has_constant(&child.as_node()));
-    } else if let Some(op) = node.as_op() {
-        if let Some(v) = op.as_value() {
-            if let Value {
-                value:
-                    SpannedMirValue {
-                        value: MirValue::Constant(_),
-                        ..
-                    },
-                ..
-            } = v.deref()
-            {
-                // This is a constant
-                return true;
-            }
-            // This is a value that is not a constant
-            return false;
-        }
-        // This is an operation that is not a value, nor a parent
-        return false;
-    }
-    // This is a stale node
-    false
-}
-
-fn debug(node: &Link<Node>) {
-    eprintln!(
-        "is_constant: {}, has_constant: {}, node: {}",
-        is_constant(node),
-        has_constant(node),
-        node.debug(),
-    );
+    is_or_has_constant(has_constant, DO_ANY, node)
 }
 
 fn as_constant(op: &Link<Op>) -> Option<u64> {
